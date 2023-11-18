b48b92ce30724b361167e86bc280d1dd768eb56e
Youssef El Mzouri <youssefmzouri@gmail.com>
2023-11-18 01:54:01 +0100

    change description of test

--- challenge_02/tests/index.test.js
+++ challenge_02/tests/index.test.js
@@ -2,19 +2,19 @@ const { challenge02 } = require('../index')
 const { largeInput } = require('../largeInput')
 
 describe('Compiler test', () => {
-  test('Output to be 4', () => {
+  test('##*& to be 4', () => {
     expect(challenge02('##*&')).toBe('4')
   })
 
-  test('Output to be 0243', () => {
+  test('&##&*&@& to be 0243', () => {
     expect(challenge02('&##&*&@&')).toBe('0243')
   })
 
-  test('Output to be 024899455', () => {
+  test('LargeInput to be 024899455', () => {
     expect(challenge02(largeInput)).toBe('024899455')
   })
 
-  test('Output to be error sending unexpected char', () => {
+  test('Throw error passing unexpected char', () => {
     expect(() => {
       challenge02('&##%&')
     }).toThrow('Invalid operation')
